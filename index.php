c2180ddc531d2ffd810752c8ab8d737e0faeb8f1
持丸直生 <naoki.mo0917@gmail.com>
2018-02-22 14:08:05 +0800

    index完成

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php 
 	echo 'Hello world';
 	ver_dump(1);
+
+	$name = 'shinya Hirai';
+
  ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+ 	<h1>Hello world</h1>
+ 
+ </body>
+ </html>
